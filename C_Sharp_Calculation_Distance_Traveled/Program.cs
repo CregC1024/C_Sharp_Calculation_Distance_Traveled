58ff19db5f0561ed28e13bd24f282a0af25603de
Creg Sullivan <cregcsullivan1024@gmail.com>
2022-01-12 01:01:26 -0600

    Add project files.

---
+++ C_Sharp_Calculation_Distance_Traveled/Program.cs
@@ -0,0 +1,36 @@
+﻿//C# program to calculate the traveled distance 
+//based on speed and time.
+
+using System;
+
+class Demo
+{
+    static int CalculatDistance(int speed, int time)
+    {
+        int distance = 0;
+
+        distance = speed * time;
+
+        return distance;
+    }
+    static void Main(string[] args)
+    {
+        int speed = 0;
+        int timeInHours = 0;
+        int distance = 0;
+
+        Console.Write("Enter time in hours: ");
+#pragma warning disable CS8604 // Possible null reference argument.
+        timeInHours = int.Parse(s: Console.ReadLine());
+#pragma warning restore CS8604 // Possible null reference argument.
+
+        Console.Write("Enter speed in miles per hour: ");
+#pragma warning disable CS8604 // Possible null reference argument.
+        speed = int.Parse(Console.ReadLine());
+#pragma warning restore CS8604 // Possible null reference argument.
+
+        distance = CalculatDistance(speed, timeInHours);
+        Console.WriteLine("Traveled distance in miles: " + distance);
+    }
+}
+
